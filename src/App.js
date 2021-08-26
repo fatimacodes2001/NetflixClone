5ff2de57414c9353d11f7c4489bc051d53532171
Fatima Tuz Zehra <57491064+fatimacodes2001@users.noreply.github.com>
2021-08-26 12:38:11 +0500

    fetched originals

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import requests from "./requests"
+import Row from './Row';
 //netflix-clone-d299e.web.app
 //11fa28afacabf123aba1cc78ffc5bbad
 
 function App() {
   return (
     <div className="App">
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
       
     </div>
   );
--- src/Row.js
+++ src/Row.js
@@ -1,8 +1,19 @@
-import React, { useState }  from "react";
+import React, { useEffect, useState }  from "react";
+import axios from "./axios";
 
 function Row(props){
     const [movies, setMovies] = useState([])
-    
+    useEffect(() => {
+
+        async function fetchData() {
+
+            const request = await axios.get(props.fetchUrl)
+
+        }
+        fetchData()
+
+    }, [])
+
     return(
         <div>
 
